8788c0121d4ea9515dedaf60b368fabb49d59c52
nadiadejgaard <nadia.dejgaard@hotmail.dk>
2020-11-11 15:30:13 +0100

    test

--- App.js
+++ App.js
@@ -6,7 +6,8 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <Text>Fuck Nadia</Text>
+      <Text>Fuck</Text>
+      <Text>Test 1</Text>
       <StatusBar style="auto" />
     </View>
   );
